249d7a33b0424725c75e4ff591d8c403909f69ed
piitur <thanat.peter@mail.kmutt.ac.th>
2024-01-25 23:07:54 +0700

    delete hash

--- next.config.js
+++ next.config.js
@@ -5,7 +5,6 @@ module.exports = {
     async redirects() {
       return [
         {
-          basePath: '/sj3/',
           source: '/',
           destination: '/home', // Your desired default path
           permanent: true,
