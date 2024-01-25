7e39d6384e17703c0db28eb62237a64f3a8cf1c3
piitur <thanat.peter@mail.kmutt.ac.th>
2024-01-25 22:40:18 +0700

    nextconfig

--- next.config.js
+++ next.config.js
@@ -5,6 +5,7 @@ module.exports = {
     async redirects() {
       return [
         {
+          basePath: '/sj3/shop',
           source: '/',
           destination: '/home', // Your desired default path
           permanent: true,
