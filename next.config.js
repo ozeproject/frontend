a3c37f317fee25fbc1bb73844c40e91b27fa6a8b
Nithi Lampay <lingchinball@MacBook-Air-khxng-Nithi.local>
2024-03-01 23:02:56 +0700

    go bcak to remotePatterns

--- next.config.js
+++ next.config.js
@@ -15,16 +15,16 @@ module.exports = {
     },
 // Test images.
     images: {
-      // remotePatterns: [
-      //   {
-      //     protocol: 'https',
-      //     hostname: 'assets.official-goods-store.jp',
-      //     port: '',
-      //     // pathname: '/product/ZMY331/73312b93dcc83560f54d904c71be419751a1ee6838303d15cd6b83b876b7f168.jpg',
-      //   },
-      // ],
-      domains: ["assets.official-goods-store.jp"],
-      formats: ["image/webp"]
+      remotePatterns: [
+        {
+          protocol: 'https',
+          hostname: 'assets.official-goods-store.jp',
+          port: '',
+          // pathname: '/product/ZMY331/73312b93dcc83560f54d904c71be419751a1ee6838303d15cd6b83b876b7f168.jpg',
+        },
+      ],
+      // domains: ["assets.official-goods-store.jp"],
+      // formats: ["image/webp"]
     },
   };
   
