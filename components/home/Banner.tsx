1f7a209fb30a4c25e05a03a2b3c9c09234ec6f45
piitur <thanat.peter@mail.kmutt.ac.th>
2024-03-17 13:33:23 +0700

    banner

--- components/home/Banner.tsx
+++ components/home/Banner.tsx
@@ -1,29 +1,61 @@
 import React, { useEffect, useState } from 'react';
-import Image from 'next/image'
+import Image from 'next/image';
 import Link from 'next/link';
 
 const Banner = () => {
-    const [productCount, setProductCount] = useState<number>(0);
+  const [productCount, setProductCount] = useState<number>(0);
+  const [currentIndex, setCurrentIndex] = useState<number>(0);
+  const images = [
+    'https://assets-global.website-files.com/5fb85f26f126ce08d792d2d9/628a5e3eebec8808b407eefc_72331280_1375222229300944_3110216239734063104_n.jpg',
+    'https://www.smartt.com/sites/default/files/2020-06/uniqlo-logo-banner.jpg',
+    'https://media.licdn.com/dms/image/C4E12AQFSyydwd-lllg/article-cover_image-shrink_600_2000/0/1520108749231?e=2147483647&v=beta&t=HzObMx8-m4eFrRgGQBqSesW4Zk2qK5jea_FPLBTfkFM'
+    // Add additional image URLs here...
+  ];
 
   useEffect(() => {
+    fetchProductCount();
+    const interval = setInterval(() => {
+      nextSlide();
+    }, 2000); // Change slide every 5 seconds
+
+    return () => clearInterval(interval);
+  }, []);
+
+  const fetchProductCount = () => {
     fetch('https://capstone23.sit.kmutt.ac.th/sj3/api/productCount')
       .then((response) => response.json())
       .then((data) => setProductCount(data.count))
       .catch((error) => console.error('Error fetching product count:', error));
-      console.log('process.env.FIREBASE_API_KEY',process.env.firebaseApiKey)
-  }, []);
- 
+  };
+
+  const nextSlide = () => {
+    setCurrentIndex((prevIndex) => (prevIndex === images.length - 1 ? 0 : prevIndex + 1));
+  };
+
+  const prevSlide = () => {
+    setCurrentIndex((prevIndex) => (prevIndex === 0 ? images.length - 1 : prevIndex - 1));
+  };
 
   return (
-    <div className=' px-20 py-10 border-b-2  border-gray-500'>
-        <div className="border-2 border-gray-500 p-2 h-96">
-        <img className='mx-auto' src="https://assets.official-goods-store.jp/product/ZMY299/babf58aafeb66622d75748d01bd85255341da3abd658eb5637b9f7c6ef4858d4.jpg" width={400} height={400} alt="Banner" loading="lazy"/>
-        </div>
-        <div className='mt-6'>
-          <div className=' font-semibold text-xl'>New Arrival</div>
-          <div className='underline text-sm mt-3'><Link href="/shop/">EXPLORE ITEMS</Link></div>
-        </div>
+    <div className='px-52 py-10 border-b-2 border-gray-500 relative'>
+    <div className='border-2 border-gray-500 p-2' style={{ height: '500px' }}>
+      <img
+        className='mx-auto object-cover'
+        src={images[currentIndex]}
+        style={{ width: '1800px', height: '480px' }}
+        alt='Banner'
+        loading='lazy'
+      />
+      <button className="absolute top-1/2 left-40 transform -translate-y-1/2 text-5xl pb-16" onClick={prevSlide}>&lt;</button>
+      <button className="absolute top-1/2 right-40 transform -translate-y-1/2 text-5xl pb-16" onClick={nextSlide}>&gt;</button>
     </div>
+    <div className='mt-6'>
+      <div className='font-semibold text-xl'>New Arrival</div>
+      <div className='underline text-sm mt-3'>
+        <Link href='/shop/'>EXPLORE ITEMS</Link>
+      </div>
+    </div>
+  </div>
   );
 };
 
