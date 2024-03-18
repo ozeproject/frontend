0a1f7f531ce3024deb0ac37ac75bfc15269e3ff0
Nithi Lampay <lingchinball@MacBook-Air-khxng-Nithi.local>
2024-03-18 21:25:30 +0700

    ย้อนเวอร์่

--- next.config.js
+++ next.config.js
@@ -9,7 +9,7 @@ module.exports = {
       return [
         {
           source: '/',
-          destination: '/sj3/home', // Your desired default path
+          destination: '/home', // Your desired default path
           permanent: true,
         },
       ];
