31ea5eed2096c883772f1bb9053835ac64b24bcc
piitur <thanat.peter@mail.kmutt.ac.th>
2024-04-28 01:28:57 +0700

    take out something

--- pages/shop/create.tsx
+++ pages/shop/create.tsx
@@ -201,9 +201,9 @@ const Create = () => {
               value={formData.gender} 
               onChange={handleInputChange} 
             >
-              <option value="Male">Male</option>
-              <option value="Female">Female</option>
-              <option value="Other">Other</option>
+              <option value="male">Male</option>
+              <option value="female">Female</option>
+              <option value="other">Other</option>
             </select>
           </div>
 
--- pages/shop/index.tsx
+++ pages/shop/index.tsx
@@ -56,11 +56,11 @@ const ShopPage = () => {
         </>
       )}
         <ProductCard />
-        <div className='  '>
+        {/* <div className='  '>
             <div className='text-center p-10 m-auto'>
                     <span className='m-auto font-semibold text-lg rounded-lg px-3 py-2 bg-[#3B3B3B] text-[#FAF9F6] border border-gray-600 text-center'>LOAD MORE . . .</span>
             </div>
-        </div>
+        </div> */}
         <Footer /> 
         
     </div>
